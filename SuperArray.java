4fd5b21e3a426067a644c6660ec0b030978064ba
carolchen1120 <72897491+carolchen1120@users.noreply.github.com>
2020-10-31 15:37:07 -0400

    Add files via upload

---
+++ SuperArray.java
@@ -0,0 +1,23 @@
+public class SuperArray {
+  private String [] data;
+  private int size; //The current size
+
+  // Creates an empty superArray.
+  // Empty does NOT mean an empty capacity, it means no elements are stored in it (the size is 0).
+  // A good starting capacity is 10.
+  public SuperArray() {
+    this.data = new String[10];
+    this.size = 0;
+  }
+
+  // Returns the number of elements in this list.
+  public int size() {
+    return this.size;
+  }
+
+  // // Appends the specified element to the end of this list
+  // public boolean add(String element) {
+  //   this.data =
+  // }
+
+}
