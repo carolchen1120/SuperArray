57b8307510f05d7f32fc0dbeb2456c7bf09aa083
carolchen1120 <72897491+carolchen1120@users.noreply.github.com>
2020-11-05 12:36:03 -0500

    Add files via upload

--- SuperArray.java
+++ SuperArray.java
@@ -144,4 +144,28 @@ public class SuperArray {
   }
 
 
+  // Create the SuperArray with the provided starting capacity.
+  public SuperArray(int initialCapacity) {
+    this.size = 0;
+    this.data = new String[initialCapacity];
+  }
+
+
+
+  //
+  // public static void removeDuplicates(SuperArray s){  }
+  // public static void main(String[]args){
+  //   SuperArray words = new SuperArray();
+  //   //grouped to save vertical space
+  //   words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
+  //   words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
+  //   words.add("una");    words.add("ebi");     words.add("toro");
+  //
+  //   System.out.println(words);
+  //   removeDuplicates(words);
+  //   System.out.println(words);
+  // }
+
+
+
 }
