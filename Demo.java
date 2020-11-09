d15658b1de73e703913fe0df2dcec5f514b269c0
carolchen1120 <72897491+carolchen1120@users.noreply.github.com>
2020-11-09 12:25:07 -0500

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -15,7 +15,23 @@ public class Demo{
   }
 
   public static SuperArray findOverlap(SuperArray a, SuperArray b) {
-    
+    String[] answer = new String[a.size];
+    String aString = "";
+    String bString = "";
+    int count = 0;
+    removeDuplicates(a);
+    removeDuplicates(b);
+    for (int i = 0; i < a.size; i++) {
+      aString = a.data[i];
+      for (int j = 0; j < b.size; j++) {
+        bString = b.data[j];
+        if (aString.equals(bString)) {
+          answer[count] = aString;
+          count++;
+        }
+      }
+    }
+    return answer;
   }
 
   public static void main(String[]args){
